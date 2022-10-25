b2d8f726d0802b6cd0a9e65651cc734f45fba8ea
Edd Sansome <edwardsansome@gmail.com>
2022-10-25 20:44:00 +0100

    basic stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def hello
+    sleep 1
+    render turbo_stream: turbo_stream.update(
+      :pages_hello,
+      nil,
+      partial: 'hello'
+    )
+  end
 end
